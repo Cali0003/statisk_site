23ba80f530f4138530fc259e81f937255cb3738a
Cali0003 <cali0003@stud.ek.dk>
2026-03-02 22:45:57 +0100

    index js

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const listContainer = document.querySelector(".card-container");
+
+const fetchUrl = `https://kea-alt-del.dk/t7/api/categories`;
+
+function getCategories() {
+  fetch(fetchUrl)
+    .then((res) => res.json())
+    .then((category) => showCategories(category));
+}
+
+function showCategories(categories) {
+  listContainer.innerHTML = "";
+
+  categories.forEach((category) => {
+    listContainer.innerHTML += `
+      
+            <a href="productlist.html?category=${category.category}" class="card-forside">
+                <h3>${category.category}</h3>
+            </a>
+    `;
+  });
+}
+
+getCategories();
